657bc6ca7a73bec88ec9e5e3a2a26179a965b590
MCarrillo99 <104209323+MCarrillo99@users.noreply.github.com>
2022-07-12 17:29:05 +0200

    Add files via upload

---
+++ myfirstAPI/src/test/java/com/develhope/myfirstAPI/MyfirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.myfirstAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
