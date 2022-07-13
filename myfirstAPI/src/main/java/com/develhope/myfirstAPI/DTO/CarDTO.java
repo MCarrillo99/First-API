5abffbf219943187c5cb70d969ee9ccc79a56880
MCarrillo99 <104209323+MCarrillo99@users.noreply.github.com>
2022-07-13 18:46:36 +0200

    Add files via upload

--- myfirstAPI/src/main/java/com/develhope/myfirstAPI/DTO/CarDTO.java
+++ myfirstAPI/src/main/java/com/develhope/myfirstAPI/DTO/CarDTO.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CarDTO {
 
+    @NotBlank
     private String id;
-
+    @NotBlank
     private String carModel;
 
     private double price;
